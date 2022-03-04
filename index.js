95417c0484e5dd3da1c06d6438909a399d6336dc
Brian Rinaldi <brian.rinaldi@gmail.com>
2022-03-04 14:43:25 -0500

    convert client to a class

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ function setShowButton(val) {
 }
 
 (async () => {
-  let user = new ldUser(await ldServer.getClient());
+  // client should be a singleton
+  const client = new ldServer();
+
+  // each user would be initialized with their info
+  let user = new ldUser(await client.getClient());
   setShowButton(await user.getFlagValue("show-button", setShowButton));
+
+  await client.closeClient();
 })();
